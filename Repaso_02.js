fd292099cba8df6bee302557a79c95cf31901c53
MISRAIM RUIZ HERNANDEZ <ruizmisraim5@gmail.com>
2024-08-26 21:57:56 -0600

    estructura de for of, for in, etc...

--- Repaso_02.js
+++ Repaso_02.js
@@ -13,11 +13,73 @@ const datosArreglo=()=>{
 }
 
 //for of
-//for in
-//for each
-//funcion flecha
-//while
+let B = [[3,4,5,6],[7,8,9]];
 
+for (let fil of B) {
+    for (let [i, val] of fil.entries()) {
+        console.log(`${i}=${val} `);
+    };
+}
+
+//for in
+let comparar = [[3,4,5,6],[7,8,9]];
+
+for (let fil of c) {
+    for (let [i, val] of fil.entries()) {
+        console.log(`${i}=${val}`);
+    }
+}
+const datosarreglo = (aray) => {
+    for (const datosindex in array) {
+        for (const valoresindex in array[datosindex]){
+            console.log(`${valoresindex}=${array[datosindex][valoresindex]}`);
+        }
+    }
+}
+
+datosarreglo(c);
+//for each
+let arreglointerno2dimensiones=[];
+
+let arreglointerno2=[[3,4,5,6],[7,8,9]];
+
+arreglointerno2dimensiones.forEach((arreglointerno2, index) => {
+    console.log(`Datos ${index + 1}:`);
+    arreglointerno2.forEach((elemento) => {
+        console.log(elemento);
+    });
+});
+//funcion flecha
+let arregloi2 =[[3,4,5,6], [7,8,9]];
+
+const leera3 = () => {
+    arregloi2.forEach((subArreglo, index) => {
+        console.log(`subarreglo ${index + 1}:`);
+        subArreglo.forEach((elemento) => {
+            console.log(elemento);
+        });
+    });
+}
+
+leera3();
+
+//while
+let b = [[1,2,3,4], [5,6,7,8]];
+
+console.log("Cliclo while");
+
+let i = 0;
+
+while (i < B.length) {
+    let v = b[i];
+    let j = 0;
+
+    while (j < v.length) {
+        console.log(v[j]);
+        j++;
+    }
+    1++;
+};
 //Estructura de control parser
 let a=4;
 let b=5;
